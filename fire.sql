ec5284eff9b87cb7607dba0c71b1917e53dd7566
jwhite0413 <67512469+jwhite0413@users.noreply.github.com>
2020-09-23 21:14:01 -0400

    Jess Upload

---
+++ fire.sql
@@ -0,0 +1,16 @@
+Select * FROM a;
+
+Select * FROM b; 
+
+Select a.id, a.name, b.other
+From a
+Inner Join id
+on a.countid = b.countid
+
+CREATE TABLE fire ph (
+id INT PRIMARY KEY,
+premise_name TEXT,
+county_id INT);
+
+CREATE TABLE  
+
